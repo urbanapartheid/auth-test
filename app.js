e1bd6aa66f7bcd162fbe7595024f93546f7643f8
Eric Chang <urbanapartheid@gmail.com>
2020-03-04 10:33:02 -0500

    Level 3 Hashing Password

--- app.js
+++ app.js
@@ -4,11 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+const md5 = require("md5");
 const app = express();
-
-console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -20,8 +17,6 @@ const userSchema = new mongoose.Schema ({
   email: String,
   password: String
 });
-
-userSchema.plugin(encrypt, { secret: process.env.secret, encryptedFields: ["password"] }); // before creating model!
 
 const User = new mongoose.model("User", userSchema);
 
@@ -40,7 +35,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err){
@@ -54,7 +49,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email : username}, function(err, foundUser){
     if (err){
