84e1157256d51d82add03cdc26a9c6ea8df9282a
Eric Chang <urbanapartheid@gmail.com>
2020-03-04 10:00:02 -0500

    Level 2 Database Encryption and gitignore

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 //jshint esversion:6
-
+require("dotenv").config(); // should be always top
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+
 const app = express();
+
+console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -18,8 +21,7 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] }); // before creating model!
+userSchema.plugin(encrypt, { secret: process.env.secret, encryptedFields: ["password"] }); // before creating model!
 
 const User = new mongoose.model("User", userSchema);
 
